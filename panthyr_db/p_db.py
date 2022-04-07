8983827dab47bad1d2b0db7f438491dc35021446
dietervansteenwegen <github@vansteenwegen.org>
2022-04-07 15:49:23 +0200

    Support for python 3.7

--- panthyr_db/p_db.py
+++ panthyr_db/p_db.py
@@ -391,7 +391,8 @@ class pDB(sqlite3.Connection):
         """
         self.execute('ATTACH DATABASE ? AS target_db', (target_db,))
         for table in table_ids:
-            if cmd := self._generate_export_cmd(table):
+            cmd = self._generate_export_cmd(table)
+            if cmd:
                 if type(cmd) == str:
                     self.execute(cmd)
                 else:
